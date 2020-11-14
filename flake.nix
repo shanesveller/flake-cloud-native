265f85ff5906e310d61032ffb59e1769e30f9903
Shane Sveller <shanesveller@gmail.com>
2020-11-14 16:35:14 -0600

    Make default package a linkfarm of all other packages

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
       packages.x86_64-linux = {
-        flux2 = pkgs.callPackage ./flux.nix {};
-        tanka = pkgs.callPackage ./tanka.nix {};
+        flux2 = pkgs.callPackage ./flux.nix { };
+        tanka = pkgs.callPackage ./tanka.nix { };
       };
 
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.tanka;
+      defaultPackage.x86_64-linux = pkgs.linkFarmFromDrvs "helm-charts-packages"
+        (builtins.attrValues self.packages.x86_64-linux);
     };
 }
