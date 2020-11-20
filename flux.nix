bd875d2e4a2e1cb9415b9c9fdc580f85831efa1a
Shane Sveller <shanesveller@gmail.com>
2020-11-20 14:54:01 -0600

    Update flux2 CLI to 0.3.0
    https://github.com/fluxcd/flux2/releases/tag/v0.3.0

--- flux.nix
+++ flux.nix
@@ -2,25 +2,25 @@
 
 buildGoModule rec {
   pname = "flux2";
-  version = "0.2.5";
+  version = "0.3.0";
 
   # https://github.com/fluxcd/flux2/releases/latest
-  # https://github.com/fluxcd/flux2/releases/tag/v0.2.5
-  # nix-prefetch-url --unpack https://github.com/fluxcd/flux2/archive/v0.2.5.tar.gz
+  # https://github.com/fluxcd/flux2/releases/tag/v0.3.0
+  # nix-prefetch-url --unpack https://github.com/fluxcd/flux2/archive/v0.3.0.tar.gz
   src = fetchFromGitHub {
     owner = "fluxcd";
     repo = "flux2";
     rev = "v${version}";
-    sha256 = "13cdd71xcs02hp17jvx2bj50lhvzy2nb83ffc5zc97imfixwfdag";
+    sha256 = "0hcxcfbkiaq06g0z5fkp0g91xl37gj5hf7ccn34zz62vij62cl5n";
   };
 
-  vendorSha256 = "KdRtD3p/jNpNgfECIIylE8umi38OELeXELgf/o0ZPy0=";
+  vendorSha256 = "TrEppwzWEkl8EKZUWq+66LlOzp0T5PZSjFZIW1Zbo48=";
 
   subPackages = [ "cmd/flux" ];
 
   doCheck = false;
 
-  # https://github.com/fluxcd/flux2/blob/v0.2.5/.goreleaser.yml#L6
+  # https://github.com/fluxcd/flux2/blob/v0.3.0/.goreleaser.yml#L6
   buildFlagsArray = [ "-ldflags=-s -w -X main.VERSION=${version}" ];
 
   nativeBuildInputs = [ installShellFiles ];
