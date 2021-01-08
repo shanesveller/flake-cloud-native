c23c02eabc33cea0ccbabe1760059208e3475c1c
Shane Sveller <shanesveller@gmail.com>
2021-01-08 09:54:52 -0600

    Update Flux2 CLI to 0.5.9

--- flux.nix
+++ flux.nix
@@ -1,27 +1,27 @@
 { buildGoModule, fetchFromGitHub, lib, installShellFiles }:
 
-let version = "0.5.7";
+let version = "0.5.9";
 in buildGoModule {
   pname = "flux2";
   inherit version;
 
   # https://github.com/fluxcd/flux2/releases/latest
-  # https://github.com/fluxcd/flux2/releases/tag/v0.5.7
-  # nix-prefetch-url --unpack https://github.com/fluxcd/flux2/archive/v0.5.7.tar.gz
+  # https://github.com/fluxcd/flux2/releases/tag/v0.5.9
+  # nix-prefetch-url --unpack https://github.com/fluxcd/flux2/archive/v0.5.9.tar.gz
   src = fetchFromGitHub {
     owner = "fluxcd";
     repo = "flux2";
     rev = "v${version}";
-    sha256 = "1xs09wamvkl8i90s73ndsni2d89s3x4xxxdmvyjxqgaqnn5bdqva";
+    sha256 = "1fic1hgsjhail9p2ki7qqr0vl5av3gfb8y3h9ygzq4j869780bsq";
   };
 
-  vendorSha256 = "L1WmdIqFI/+/r7aBXwflgRE/owOeyy8c0jWJ0HAxtuQ=";
+  vendorSha256 = "m2GM5htbPJNgjQnVkQ1BL6cTKJH1ulQ79oDnEC8g9pc=";
 
   subPackages = [ "cmd/flux" ];
 
   doCheck = false;
 
-  # https://github.com/fluxcd/flux2/blob/v0.5.7/.goreleaser.yml#L6
+  # https://github.com/fluxcd/flux2/blob/v0.5.9/.goreleaser.yml#L6
   buildFlagsArray = [ "-ldflags=-s -w -X main.VERSION=${version}" ];
 
   nativeBuildInputs = [ installShellFiles ];
