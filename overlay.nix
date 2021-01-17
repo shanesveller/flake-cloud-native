ea66345b50b3701542aac84ecf88c708650e509c
Shane Sveller <shanesveller@gmail.com>
2021-01-17 09:57:15 -0600

    Add package output for building all binaries

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,7 @@
 final: prev: {
   cloud-native = {
+    all = prev.linkFarmFromDrvs "flake-cloud-native"
+      (with final.cloud-native; [ flux2 nova tanka tkn ]);
     flux2 = prev.callPackage ./flux.nix { };
     nova = prev.callPackage ./nova.nix { };
     tanka = prev.callPackage ./tanka.nix { };
