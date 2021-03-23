7717ebfd24473efd1c54cd867243b258c752889d
Shane Sveller <shanesveller@gmail.com>
2021-03-23 14:18:37 -0500

    Re-export Flux2 CLI from nixpkgs-unstable

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,7 @@ final: prev: {
   cloud-native = {
     all = prev.linkFarmFromDrvs "flake-cloud-native"
       (with final.cloud-native; [ flux2 nova tanka tkn ]);
-    flux2 = prev.callPackage ./flux.nix { };
+    flux2 = prev.fluxcd;
     nova = prev.callPackage ./nova.nix { };
     tanka = prev.callPackage ./tanka.nix { };
     tkn = prev.callPackage ./tkn.nix { };
