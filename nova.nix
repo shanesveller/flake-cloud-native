cc5b028d39bd06f39fff1f8da244fb27208fb283
Shane Sveller <shanesveller@gmail.com>
2021-04-05 12:57:51 -0500

    Update Nova CLI to 2.3.1

--- nova.nix
+++ nova.nix
@@ -1,17 +1,17 @@
 { buildGoModule, fetchFromGitHub, lib }:
-let version = "2.3.0";
+let version = "2.3.1";
 in buildGoModule {
   pname = "nova";
   inherit version;
 
   # https://github.com/FairwindsOps/nova/releases/latest
-  # https://github.com/FairwindsOps/nova/releases/tag/2.3.0
-  # nix-prefetch-url --unpack https://github.com/FairwindsOps/nova/archive/2.3.0.tar.gz
+  # https://github.com/FairwindsOps/nova/releases/tag/2.3.1
+  # nix-prefetch-url --unpack https://github.com/FairwindsOps/nova/archive/2.3.1.tar.gz
   src = fetchFromGitHub {
     owner = "FairwindsOps";
     repo = "nova";
     rev = version;
-    sha256 = "0c8bm406r44s8nxw41xdch04r1q1andpbq4lgy9h2nhfbcd48bsv";
+    sha256 = "146l3ggrz6bz2mkmsr4807rsxalvfylyzj2myhsrgd1v64b5hfgj";
   };
 
   vendorSha256 = "2DK5P0szp9R9jt7MYJZB+gV8DwWrsrG3BOvDekN7cbI=";
